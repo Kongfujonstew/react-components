98b0c73bc9420b43ca6f431ee96e226783fd6c91
Kongfujonstew <jonstew@gmail.com>
2017-04-12 11:18:03 -0700

    render list

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// TODO
+var GroceryList = () => (
+  <div>
+  <h1>My To Do List</h1>
+  <ToDoList todos={['yellow ledbetter', 'instate lovesong', 'h']}/>
+  </div>
+);
+
+var ToDoList = (props) => (
+  <div>
+  <ul>ListItems:
+
+    var todos = props.todos;
+    <li>{props.todos[0]}</li> 
+    <li>{props.todos[1]}</li> 
+    <GroceryListItem todo={props.todos[2]}/> 
+
+  </ul>
+  </div>
+
+)
+
+var GroceryListItem = (props) => (
+  <li>{props.todo}</li>
+
+
+)
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
